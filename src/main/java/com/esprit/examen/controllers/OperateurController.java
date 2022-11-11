80aa2ac41e1f376a5c90a033c6f128983d9abf1a
moementrabelsi <44821658+moementrabelsi@users.noreply.github.com>
2022-11-11 23:25:15 +0100

    change

--- src/main/java/com/esprit/examen/controllers/OperateurController.java
+++ src/main/java/com/esprit/examen/controllers/OperateurController.java
@@ -1,8 +1,7 @@
 package com.esprit.examen.controllers;
 
 import java.util.List;
-import com.esprit.examen.dto.*;
-import org.modelmapper.ModelMapper;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import com.esprit.examen.entities.Operateur;
@@ -15,9 +14,6 @@ import io.swagger.annotations.Api;
 @RequestMapping("/operateur")
 @CrossOrigin("*")
 public class OperateurController {
-
-	@Autowired
-	private ModelMapper mapper ;
 
 	@Autowired
 	IOperateurService operateurService;
@@ -40,10 +36,9 @@ public class OperateurController {
 	// http://localhost:8089/SpringMVC/operateur/add-operateur
 	@PostMapping("/add-operateur")
 	@ResponseBody
-	public Operateur addOperateur(@RequestBody OperateurDTO op) {
-
-		Operateur ope=mapper.map(op,Operateur.class);
-		return operateurService.addOperateur(ope);
+	public Operateur addOperateur(@RequestBody Operateur op) {
+		Operateur operateur = operateurService.addOperateur(op);
+		return operateur;
 	}
 
 	// http://localhost:8089/SpringMVC/operateur/remove-operateur/{operateur-id}
@@ -56,9 +51,8 @@ public class OperateurController {
 	// http://localhost:8089/SpringMVC/operateur/modify-operateur
 	@PutMapping("/modify-operateur")
 	@ResponseBody
-	public Operateur modifyOperateur(@RequestBody OperateurDTO operateur) {
-		Operateur ope=mapper.map(operateur,Operateur.class);
-		return operateurService.updateOperateur(ope);
+	public Operateur modifyOperateur(@RequestBody Operateur operateur) {
+		return operateurService.updateOperateur(operateur);
 	}
 
 	
