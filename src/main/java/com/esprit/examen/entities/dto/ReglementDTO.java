73e8433060592ff6c5d7b327d009e8a3867eb02c
onskechrid <onskechrid1999@gmail.com>
2022-11-12 09:59:06 +0100

    fix sonar errors

---
+++ src/main/java/com/esprit/examen/entities/dto/ReglementDTO.java
@@ -0,0 +1,21 @@
+package com.esprit.examen.entities.dto;
+
+import com.esprit.examen.entities.Facture;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ReglementDTO {
+    private Long idReglement;
+    private float montantPaye;
+    private float montantRestant;
+    private Boolean payee;
+    private Date dateReglement;
+    private Facture facture;
+}
