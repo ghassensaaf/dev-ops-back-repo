1c273a23e44b11e2b786ef71bd8c38d4c3b8c267
onskechrid <onskechrid1999@gmail.com>
2022-11-14 15:14:12 +0100

    remove unused imports

--- src/main/java/com/esprit/examen/repositories/ReglementRepository.java
+++ src/main/java/com/esprit/examen/repositories/ReglementRepository.java
@@ -1,11 +1,6 @@
 package com.esprit.examen.repositories;
 
-import java.util.Date;
-import java.util.List;
-
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.esprit.examen.entities.Reglement;
